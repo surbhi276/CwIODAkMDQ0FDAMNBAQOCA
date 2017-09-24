138df6e7fd0cd9bcb9a3cedb7db132e39f1657fd
surbhi276 <surbhisingh612@gmail.com>
2017-09-24 20:08:05 +0530

    MAJOR COMMIT 2

--- logs_module/config.js
+++ logs_module/config.js
@@ -1,10 +1,10 @@
 {
-	"exceptionFileName": "exception_log_file.log",
-	"logFileName": "log_file.log",
-	"logFileMaxSize": "20480000",
-	"logFileDateFormat": ".yyyy-MM-dd",
-	"consoleLogLevel": "none",
-	"fileLogLevel": "error",
-	"logFolder": "logs",
-	"backupLogFolder": "backup_logs"
+    "exception_file_name": "exception_log_file.log",
+	"log_file_name": "log_file.log",
+	"log_file_max_size": "20480000",
+	"log_file_date_format": ".yyyy-MM-dd",
+	"console_log_level": "none",
+	"file_log_level": "error",
+	"log_folder": "logs",
+	"backup_log_folder": "backup_logs"
 }
